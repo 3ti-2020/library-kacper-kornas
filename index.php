769fda16a84e200f81e210a9b2d752003d92e481
Dixxu <62933020+Dixxu@users.noreply.github.com>
2020-05-28 07:11:38 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="grid-container">
+    <div class="box box1">
 <?php
-echo("start");
+$conn=new mysqli('localhost','kornas','123','lib');
+$sql=('SELECT * FROM autor,tytul,krzyzowa WHERE (autor.id_autor = krzyzowa.id_autor) AND (tytul.id_tytul = krzyzowa.id_tytul)');
+$result=$conn->query($sql);
+
+echo("<table>");
+echo("<tr>
+<th>Imie</th>
+<th>Nazwisko</th>
+<th>Tytul</th>
+<th>ISBN</th>
+</tr>");
+    while($wiersz=$result->fetch_assoc()){
+        echo("<tr>");
+        echo("<td>".$wiersz['imie']."</td><td>".$wiersz['nazwisko']."</td><td>".$wiersz['tytul']."</td><td>".$wiersz['isbn']."</td>");
+        echo("</tr>");
+ }
+echo("</table>");
+
 ?>
+    
+    </div>
+    <div class="box box2">
+    <form action="insert.php" method="post" class="insert">
+                    <input type="text" name="imie" placeholder="Imię">
+                    <input type="text" name="nazwisko" placeholder="Nazwisko">
+                    <input type="text" name="tytul" placeholder="tytul">
+                    <input type="text" name="isbn" placeholder="ISBN">
+                    <input type="submit" value="Dodaj">
+                </form>
+    </div>
+    <div class="box box3">3</div>
+    <div class="box box4">4</div>
+    <div class="box box5">5</div>
+    </div>
+</body>
+</html>
--- lib.sql
+++ lib.sql
@@ -1,1 +1,149 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 28 Maj 2020, 06:52
+-- Wersja serwera: 10.4.8-MariaDB
+-- Wersja PHP: 7.3.10
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `lib`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `autor`
+--
+
+CREATE TABLE `autor` (
+  `id_autor` int(11) NOT NULL,
+  `imie` varchar(25) COLLATE utf8mb4_polish_ci NOT NULL,
+  `nazwisko` varchar(25) COLLATE utf8mb4_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `autor`
+--
+
+INSERT INTO `autor` (`id_autor`, `imie`, `nazwisko`) VALUES
+(1, 'Adam', 'Mickiewicz'),
+(2, 'Ignacy', 'Krasicki'),
+(3, 'Paulina', 'Świst'),
+(4, 'Wojciech', 'Chmielarz');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `krzyzowa`
+--
+
+CREATE TABLE `krzyzowa` (
+  `id_krzyz` int(11) NOT NULL,
+  `id_autor` int(5) NOT NULL,
+  `id_tytul` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `krzyzowa`
+--
+
+INSERT INTO `krzyzowa` (`id_krzyz`, `id_autor`, `id_tytul`) VALUES
+(1, 1, 2),
+(2, 2, 3),
+(3, 3, 4),
+(4, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tytul`
+--
+
+CREATE TABLE `tytul` (
+  `id_tytul` int(11) NOT NULL,
+  `tytul` varchar(50) COLLATE utf8mb4_polish_ci NOT NULL,
+  `isbn` int(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Zrzut danych tabeli `tytul`
+--
+
+INSERT INTO `tytul` (`id_tytul`, `tytul`, `isbn`) VALUES
+(1, 'Podpalacz', 979),
+(2, 'Dziady cz.3', 978),
+(3, 'Pijaństwo', 544),
+(4, 'Przekręt', 655);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`id_autor`);
+
+--
+-- Indeksy dla tabeli `krzyzowa`
+--
+ALTER TABLE `krzyzowa`
+  ADD PRIMARY KEY (`id_krzyz`) USING BTREE,
+  ADD KEY `id_autor` (`id_autor`);
+
+--
+-- Indeksy dla tabeli `tytul`
+--
+ALTER TABLE `tytul`
+  ADD PRIMARY KEY (`id_tytul`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+
+--
+-- AUTO_INCREMENT dla tabeli `krzyzowa`
+--
+ALTER TABLE `krzyzowa`
+  MODIFY `id_krzyz` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT dla tabeli `tytul`
+--
+ALTER TABLE `tytul`
+  MODIFY `id_tytul` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `krzyzowa`
+--
+ALTER TABLE `krzyzowa`
+  ADD CONSTRAINT `krzyzowa_ibfk_1` FOREIGN KEY (`id_autor`) REFERENCES `autor` (`id_autor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
