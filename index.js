70c90d6c280d96210173568cfd50377c3fd2975f
Dixxu <62933020+Dixxu@users.noreply.github.com>
2020-06-02 02:11:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".btn");
+const btn2 = document.querySelector(".btn2");
+
+btn.addEventListener("click", function(){
+    console.log("tu będzie podstrona");
+});
+
+
+btn2.addEventListener("click", function(){
+    console.log("tu bedzie zmiana koloru diva");
+});
--- index.php
+++ index.php
@@ -1,48 +1,62 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
+<html>
+  <head>
+    <title> Kacper Kornas</title>
+    <meta charset="UTF-8" />
     <link rel="stylesheet" href="style.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <div class="grid-container">
-    <div class="box box1">
-<?php
-$conn=new mysqli('localhost','kornas','123','lib');
-$sql=('SELECT * FROM autor,tytul,krzyzowa WHERE (autor.id_autor = krzyzowa.id_autor) AND (tytul.id_tytul = krzyzowa.id_tytul)');
-$result=$conn->query($sql);
+  </head>
 
-echo("<table>");
-echo("<tr>
-<th>Imie</th>
-<th>Nazwisko</th>
-<th>Tytul</th>
-<th>ISBN</th>
-</tr>");
-    while($wiersz=$result->fetch_assoc()){
-        echo("<tr>");
-        echo("<td>".$wiersz['imie']."</td><td>".$wiersz['nazwisko']."</td><td>".$wiersz['tytul']."</td><td>".$wiersz['isbn']."</td>");
-        echo("</tr>");
- }
-echo("</table>");
+  <body>
+  <h1>Kacper Kronas</h1>
+  <div class="grid-container">
+<div class="box box1">
 
-?>
+<div class="btn"><a href="Samochody.html">Najszybszy Samochód Świata</a></div>
+<div class="btn2"><a href="Samoloty.html">Najszybszy Samolot Świata</a></div>
+
+</div>
+<div class="box box2">coś</div>
+<div class="box box3">
+  <form class="calculator" name="kalk">
+    <input class="value" type="text" name="txt" readonly="">
+    <span class="num" onclick="document.kalk.txt.value =''">c</span>
+    <span class="num" onclick="document.kalk.txt.value +='/'">/</span>
+    <span class="num" onclick="document.kalk.txt.value +='*'">*</span>
+    <span class="num" onclick="document.kalk.txt.value +='-'">-</span>
+    <span class="num" onclick="document.kalk.txt.value +='7'">7</span>
+    <span class="num" onclick="document.kalk.txt.value +='8'">8</span>
+    <span class="num" onclick="document.kalk.txt.value +='9'">9</span>
+    <span class="num" onclick="document.kalk.txt.value +='+'">+</span>
+    <span class="num" onclick="document.kalk.txt.value +='4'">4</span>
+    <span class="num" onclick="document.kalk.txt.value +='5'">5</span>
+    <span class="num" onclick="document.kalk.txt.value +='6'">6</span>
+    <span class="num" onclick="document.kalk.txt.value +='.'">.</span>
+    <span class="num" onclick="document.kalk.txt.value +='1'">1</span>
+    <span class="num" onclick="document.kalk.txt.value +='2'">2</span>
+    <span class="num" onclick="document.kalk.txt.value +='3'">3</span>
+    <span class="num" onclick="document.kalk.txt.value = eval(document.kalk.txt.value)">=</span>
     
-    </div>
-    <div class="box box2">
-    <form action="insert.php" method="post" class="insert">
-                    <input type="text" name="imie" placeholder="Imię">
-                    <input type="text" name="nazwisko" placeholder="Nazwisko">
-                    <input type="text" name="tytul" placeholder="tytul">
-                    <input type="text" name="isbn" placeholder="ISBN">
-                    <input type="submit" value="Dodaj">
-                </form>
-    </div>
-    <div class="box box3">3</div>
-    <div class="box box4">4</div>
-    <div class="box box5">5</div>
-    </div>
-</body>
+  </form>
+</div>
+<div class="box box4">
+  <?php
+            $conn=new mysqli('localhost','kornas','123','kornas');
+            $sql=('SELECT imie, dzial FROM prac,dzial WHERE(dzial.id = prac.id_dzial)');
+            $result=$conn->query($sql);
+            echo("<table>");
+            echo("<tr>
+            <th>Imie</th>
+            <th>Dzial</th>
+            </tr>");
+                while($wiersz=$result->fetch_assoc()){
+                    echo("<tr>");
+                    echo("<td>".$wiersz['imie']."</td><td>".$wiersz['dzial']."</td>");
+                    echo("</tr>");
+             }
+            echo("</table>");
+            ?>
+            </div>
+    <script src="index.js"></script>
+</div>
+  </body>
 </html>
