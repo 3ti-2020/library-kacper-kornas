af538d728e0e3517d211fca691f22c4d6818f9ed
Dixxu <62933020+Dixxu@users.noreply.github.com>
2020-06-07 01:07:45 +0200

    Add files via upload

--- apk.js
+++ apk.js
@@ -1,25 +1,26 @@
 function smoothScroll(target,duration){
     var target = document.querySelector(target);
     var targetPosition = target.getBoundingClientRect().top;
-    var startPostion = window.pageYOffset;
-    var distance = targetPosition - startPostion;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
     var startTime = null;
     
 
+    console.log(target);
+    console.log(targetPosition);
+    console.log(distance);
     function animation(currentTime) {
         if(startTime === null ) startTime = currentTime;
         var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPostion, distance, duration);
+        var run = ease(timeElapsed, startPosition, distance, duration);
         window.scrollTo(0,run);
         if(timeElapsed < duration) requestAnimationFrame(animation);
     }
 
 
     function ease(t, b, c, d){
-        t /= d/2;
-        if (t < 1) return c/2*t*t + b;
-        t--;
-        return -c/2 * (t*(t-2) - 1) + b;
+	t /= d;
+	return c*t*t*t*t*t + b;
     };
 
 
@@ -28,11 +29,24 @@ function smoothScroll(target,duration){
 
 var s1 = document.querySelector('.s1');
 var s2 = document.querySelector('.s2');
+var s3 = document.querySelector('.s3');
+var s4 = document.querySelector('.s4');
+var box1 = document.querySelector('.box1');
+var box2 = document.querySelector('.box2');
+var box3 = document.querySelector('.box3');
 
 s1.addEventListener('click', function(){
-    smoothScroll('.s2', 1000);
+    smoothScroll('.box3', 1000);
 });
 
 s2.addEventListener('click', function(){
-    smoothScroll('.s1', 1000);
+    smoothScroll('.box2', 1000);
+});
+
+s3.addEventListener('click', function(){
+    smoothScroll('.box1', 1000);
+});
+
+s4.addEventListener('click', function(){
+    smoothScroll('.box1', 1000);
 });
