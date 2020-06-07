34dd3d2724ac68c525e45a2fa13aa4c59aa078b3
Dixxu <62933020+Dixxu@users.noreply.github.com>
2020-06-07 23:04:30 +0200

    Add files via upload

--- jsVE.js
+++ jsVE.js
@@ -1,5 +1,6 @@
 function scrollAppear(){
     var text = document.querySelector('.text');
+    var img = document.querySelector('.img');
     var position = text.getBoundingClientRect().top;
     var screenPosition = window.innerHeight / 1.7;
 
@@ -11,3 +12,31 @@ function scrollAppear(){
 }
 
  window.addEventListener('scroll',scrollAppear);
+
+
+
+ var i = 0;
+ var images = [];
+ var time = 3000;
+
+
+
+ images[0] = 'img0.jpg';
+ images[1] = 'img1.jpg';
+ images[2] = 'img2.jpg';
+ images[3] = 'img3.jpg';
+
+ function changeIMG(){
+     document.slide.src = images[i];
+
+
+     if (i < images.length - 1) {
+         i++
+     } else {
+         i = 0
+     }
+
+     setTimeout("changeIMG()", time);
+ }
+
+ window.onload = changeIMG;
